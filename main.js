242b93af0cd79964e0de3f50389bd84c1e242f8d
Yalalt <yalaltgandush@gmail.com>
2023-01-31 10:14:31 +0800

    fs added

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const cors = require("cors");
 const bodyParser = require("body-parser");
+const fs = require("fs");
 
 const app = express();
 const PORT = 3008;
